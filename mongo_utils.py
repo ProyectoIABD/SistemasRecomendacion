db391535598bfb5461d3ef397026aca3ef75e06a
Josh <joshua.winn.2001@gmail.com>
2025-06-04 18:07:31 +0200

    Correccion del anterior commit

--- mongo_utils.py
+++ mongo_utils.py
@@ -1,12 +1,18 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Cambia la URI según tu configuración de MongoDB
-MONGO_URI = 'mongodb://localhost:27017/'
-DB_NAME = 'recomendador'
+DB_NAME = 'Recomendaciones'
 COLLECTION = 'usuarios'
 
-client = MongoClient(MONGO_URI)
+DB_peliculas = 'Recomendaciones'
+COLLECTION_peliculas = 'movie_data'
+
+
+uri = "mongodb+srv://AlumnoLinkia:holi1234@sistemarecomendacion.wqyrle5.mongodb.net/?retryWrites=true&w=majority&appName=SistemaRecomendacion"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
 db = client[DB_NAME]
 usuarios_col = db[COLLECTION]
 
